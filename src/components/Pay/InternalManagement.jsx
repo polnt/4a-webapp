c262f32b72358f6cf365591179fda2e773f8847e
Pol <polcahh@gmail.com>
2021-09-11 16:52:37 +0200

    update content

--- src/components/Pay/InternalManagement.jsx
+++ src/components/Pay/InternalManagement.jsx
@@ -10,7 +10,10 @@ const InternalManagement = () => {
           personnel »
         </p>
         <section>
-          <h2>Accompagnement de votre personnel :</h2>
+          <h2>
+            Pourquoi choisir l’accompagnement de 4A Paie RH Conseils pour votre
+            gestion interne?
+          </h2>
           <p>
             Dans une PME, la gestion du personnel et de la paie est très souvent
             assumée par la même personne.
@@ -23,32 +26,46 @@ const InternalManagement = () => {
             votre convention collective applicable.
           </p>
           <p>
-            Nous pouvons accompagner votre salarié sur une thématique précise
-            selon le temps nécessaire et acceptable sans gêner l’organisation
-            interne, nous nous déplaçons sur site.
+            Nous assurons la mise à jour de l’évolution législative de la paie,
+            des procédures et la vérification de la justesse de vos bulletins.
           </p>
           <p>
-            Nous pouvons être à ses côtés en cas de besoin, de conseils afin de
-            sécuriser une procédure ou une période de bulletins ou de charges
-            sociales.
+            Vous gardez la main en vous assurant les compétences d’un
+            professionnel spécialisé en paie.
           </p>
         </section>
         <section>
-          <h2>Formation de votre personnel :</h2>
-          <p>
-            Besoin d’une montée en compétence en interne ? Nous assurons la
-            formation de vos collaborateurs.
-          </p>
-          <p>
-            Nous venons dans votre entreprise, à leurs côtés pour travailler sur
-            des dossiers en cours, sur une thématique particulière, cela peut
-            être 2h, une demi-journée voire une journée en fonction de leurs
-            besoins (congés payés, maladie, heures supplémentaires et
-            complémentaires…)
-          </p>
+          <h2>Notre collaboration</h2>
+          <section>
+            <h3>Accompagnement de votre personnel :</h3>
+            <p>
+              Nous accompagnons votre salarié sur une thématique précise selon
+              le temps nécessaire et acceptable sans gêner l’organisation
+              interne, nous nous déplaçons sur site.
+            </p>
+            <p>
+              Nous sommes à vos côtés en cas de besoin, de conseils afin de
+              sécuriser une procédure ou une période de bulletins ou de charges
+              sociales.
+            </p>
+          </section>
+          <section>
+            <h3>Formation de votre personnel :</h3>
+            <p>
+              Besoin d’une montée en compétence en interne ? Nous assurons la
+              formation de vos collaborateurs.
+            </p>
+            <p>
+              Nous venons dans votre entreprise, à leurs côtés pour travailler
+              sur des dossiers en cours, sur une thématique particulière, cela
+              peut être 2h, une demi-journée voire une journée en fonction de
+              leurs besoins (congés payés, maladie, heures supplémentaires et
+              complémentaires…)
+            </p>
+          </section>
         </section>
         <section>
-          <h2>Vérification et contrôle périodique :</h2>
+          <h3>Vérification et contrôle périodique :</h3>
           <p>
             Vous avez des interrogations concernant vos bulletins ou charges
             sociales, nous pouvons vous aider à comprendre et à vérifier
@@ -62,10 +79,10 @@ const InternalManagement = () => {
           </p>
         </section>
         <section>
-          <h2>
+          <h3>
             Les avantages d’un accompagnement personnalisé en cas de gestion
             interne
-          </h2>
+          </h3>
           <p>
             Vous conservez votre contrôle sur votre service Paie RH tout en
             améliorant votre expertise et assurer la justesse de vos paies et
